6dffa1ea7353dbc0b6bdfe2788637687318c08cb
Doniyor <umidshoot@gmail.com>
2020-03-21 18:07:16 +0500

    feat: add changes dom

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const NAVIGATION = 
+//link
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        const blockID = anchor.getAttribute('href').substr(1);
+        document.getElementById(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+        for (let anchor of anchors) {
+            anchor.removeAttribute("class", "active");
+        }
+        anchor.setAttribute("class", "active");
+    })
+}
